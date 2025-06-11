ebf11c4e9a799df59f2634361f658798442190fa
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-06-11 11:13:11 +0100

    feat: update App component layout and integrate Headerbsb

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className='bg-red-900'>===</div>
-  )
-}
+    <div className="flex flex-col items-center">
+      <Header />
+    </div>
+  );
+};
 
-export default App
+export default App;
